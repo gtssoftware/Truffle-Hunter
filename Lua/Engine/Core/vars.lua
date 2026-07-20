247ac9991a8d67248174bc40c714bb21387bba9a
coolkit1 <christinafrias24@gmail.com>
2026-07-19 17:38:39 -0700

    Update vars.lua

--- Lua/Engine/Core/vars.lua
+++ Lua/Engine/Core/vars.lua
@@ -1,2 +1,2 @@
--- CAREFUL AMIGO! This system stores the vars that are requires, and also its a pretty stupid fucking system. THIS MAY BACKFIRE ON US
+-- CAREFUL AMIGO! This system stores the vars that are required, and also its a pretty stupid fucking system. THIS MAY BACKFIRE ON US
 truffles = 0
